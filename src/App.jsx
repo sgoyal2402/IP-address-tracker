f3c74d646ddaff6e84e168e3184c407e1c8e4950
Suryansh <sgoyal2402@gmail.com>
2021-05-06 00:17:11 +0530

    center fixing

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
   }
 
   return (
-    <div className="container">
+    <div className="container text-center">
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
         alt="Loading"
